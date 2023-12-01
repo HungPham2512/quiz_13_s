4a72c7c89e0068011ff663d7fd952bf74fc9d101
HungPham2512 <hungpham25122001@gmail.com>
2023-12-01 16:51:23 +0700

    updated

--- src/exception.cpp
+++ src/exception.cpp
@@ -1,58 +1,90 @@
 #include "../header/exception.h"
 
-    bool Exception::isValidName(const std::string& name) {
+    bool Exception::isValidName( std::string& name) {
         return std::regex_match(name, std::regex("[A-Za-z ]+"));
     }
 
-    bool Exception::isValidBirthDayFormat(const std::string& birthDay) {
+    bool Exception::isValidBirthDayFormat( std::string& birthDay) {
         return std::regex_match(birthDay, std::regex(R"(\d{2}/\d{2}/\d{4})"));
     }
 
-    bool Exception::isValidPhoneNumber(const std::string& phone) {
+    bool Exception::isValidPhoneNumber( std::string& phone) {
         return std::all_of(phone.begin(), phone.end(), ::isdigit);
     }
 
-    bool Exception::isValidEmailFormat(const std::string& email) {
+    bool Exception::isValidEmailFormat( std::string& email) {
         return std::regex_match(email, std::regex(R"(\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b)"));
     }
 
-    void Exception::nameException(const std::string& name) {
+    void Exception::nameException(std::string& name) {
+    bool isValid = isValidName(name);
+    if(!isValid){std::cout << "Exception occurred: Invalid Name Exception" << std::endl;}
+    while(!isValid) {
     try {
-        if (!isValidName(name)) {
-            throw "Invalid Name Exception";
+            std::cout << "Enter a valid name: ";
+            std::getline(std::cin, name);
+            if (!isValidName(name)) {
+                throw "Invalid Name Exception";
+            }
+            else {isValid = true;}
+        } catch (const char* msg) {
+            std::cout << "Exception occurred: " << msg << std::endl;
+            std::cout << "Please try again." << std::endl;
         }
-    } catch (const char* msg) {
-        std::cerr << "Exception occurred: " << msg << std::endl;
     }
     }
 
-    void Exception::birthdayException(const std::string& birthDay) {
+    void Exception::birthdayException(std::string& birthDay) {
+    bool isValid = isValidBirthDayFormat(birthDay);
+    if(!isValid){std::cout << "Exception occurred: Invalid BirthDay Exception" << std::endl;}
+    while(!isValid) {
     try {
-        if (!isValidBirthDayFormat(birthDay)) {
-            throw "Invalid Birthday Format Exception";
+            std::cout << "Enter a valid birthDay: ";
+            std::getline(std::cin, birthDay);
+            if (!isValidBirthDayFormat(birthDay)) {
+                throw "Invalid BirthDay Exception";
+            }
+            else {isValid = true;}
+        } catch (const char* msg) {
+            std::cout << "Exception occurred: " << msg << std::endl;
+            std::cout << "Please try again." << std::endl;
         }
-    } catch (const char* msg) {
-        std::cerr << "Exception occurred: " << msg << std::endl;
     }
     }
 
-    void Exception::phoneException(const std::string& phone) {
+    void Exception::phoneException(std::string& phone) {
+    bool isValid = isValidPhoneNumber(phone);
+    if(!isValid){std::cout << "Exception occurred: Invalid Phone number Exception" << std::endl;}
+    while(!isValid) {
     try {
-        if (!isValidPhoneNumber(phone)) {
-            throw "Invalid Phone Number Exception";
+            std::cout << "Enter a valid Phone number: ";
+            std::getline(std::cin, phone);
+            if (!isValidPhoneNumber(phone)) {
+                throw "Invalid Phone number Exception";
+            }
+            else {isValid = true;}
+        } catch (const char* msg) {
+            std::cout << "Exception occurred: " << msg << std::endl;
+            std::cout << "Please try again." << std::endl;
         }
-    } catch (const char* msg) {
-        std::cerr << "Exception occurred: " << msg << std::endl;
     }
     }
 
-    void Exception::mailException(const std::string& email) {
+    void Exception::mailException(std::string& email) {
+    bool isValid = isValidEmailFormat(email);
+    if(!isValid){std::cout << "Exception occurred: Invalid Email Exception" << std::endl;}
+    while(!isValid) {
     try {
-        if (!isValidEmailFormat(email)) {
-            throw "Invalid Email Format Exception";
+            std::cout << "Enter a valid Email: ";
+            std::getline(std::cin, email);
+            if (!isValidEmailFormat(email)) {
+                throw "Invalid Email Exception";
+            }
+            else {isValid = true;}
+        } catch (const char* msg) {
+            std::cout << "Exception occurred: " << msg << std::endl;
+            std::cout << "Please try again." << std::endl;
         }
-    } catch (const char* msg) {
-        std::cerr << "Exception occurred: " << msg << std::endl;
     }
     }
 
